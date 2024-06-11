952857d2cae310c53ac4cdb9b4ac9a7cc901c2d8
HemersonGarcias <71848645+HemersonGarcias@users.noreply.github.com>
2024-06-11 20:19:12 -0300

    responsivity fix

--- project-my-page/script.js
+++ project-my-page/script.js
@@ -4,17 +4,17 @@ const paragrafos = document.querySelectorAll(".grid-section p");
 console.log(links.length);
 console.log(paragrafos.length);
 
-paragrafos[0].classList.add("ativo")
+paragrafos[0].classList.add("ativo");
 
-function activeLink(index){
-    paragrafos.forEach((paragrafo)=>{
-        paragrafo.classList.remove("ativo")
-    })
-    paragrafos[index].classList.add("ativo");
+function activeLink(index) {
+  paragrafos.forEach((paragrafo) => {
+    paragrafo.classList.remove("ativo");
+  });
+  paragrafos[index].classList.add("ativo");
 }
 
-links.forEach((link, index)=>{
-    link.addEventListener('click',()=>{
-        activeLink(index);
-    })
-})
+links.forEach((link, index) => {
+  link.addEventListener("click", () => {
+    activeLink(index);
+  });
+});
