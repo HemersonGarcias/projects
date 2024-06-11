b73e92de5135013bebd004f14ac06b111c19afa2
HemersonGarcias <71848645+HemersonGarcias@users.noreply.github.com>
2024-06-10 23:21:13 -0300

    Add files via upload

---
+++ project-my-page/script.js
@@ -0,0 +1,20 @@
+const links = document.querySelectorAll("a");
+const paragrafos = document.querySelectorAll(".grid-section p");
+
+console.log(links.length);
+console.log(paragrafos.length);
+
+paragrafos[0].classList.add("ativo")
+
+function activeLink(index){
+    paragrafos.forEach((paragrafo)=>{
+        paragrafo.classList.remove("ativo")
+    })
+    paragrafos[index].classList.add("ativo");
+}
+
+links.forEach((link, index)=>{
+    link.addEventListener('click',()=>{
+        activeLink(index);
+    })
+})
