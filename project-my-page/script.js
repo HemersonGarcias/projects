e50d605ab9cbb4c0c03867f09f5efe701794c170
HemersonGarcias <71848645+HemersonGarcias@users.noreply.github.com>
2024-06-12 18:03:40 -0300

    align-items !fix

--- project-my-page/script.js
+++ project-my-page/script.js
@@ -1,6 +1,5 @@
 const links = document.querySelectorAll("a");
 const paragrafos = document.querySelectorAll(".grid-section p");
-const imagens = document.querySelector(".imgs-social")
 
 console.log(links.length);
 console.log(paragrafos.length);
@@ -11,13 +10,8 @@ paragrafos[0].classList.add("ativo");
 function activeLink(index) {
   paragrafos.forEach((paragrafo) => {
     paragrafo.classList.remove("ativo");
-    imagens.classList.remove("ativo")
   });
   paragrafos[index].classList.add("ativo");
-  if(paragrafos[paragrafos.length-1].classList.contains("ativo")){
-    imagens.classList.add("ativo")
-    //alert("awd")
-  }
 }
 
 links.forEach((link, index) => {
