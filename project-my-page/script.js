d82b18b202bc1a532ee0731df663f7257b6f4a5a
HemersonGarcias <71848645+HemersonGarcias@users.noreply.github.com>
2024-06-11 21:53:25 -0300

    add social contact

--- project-my-page/script.js
+++ project-my-page/script.js
@@ -1,16 +1,23 @@
 const links = document.querySelectorAll("a");
 const paragrafos = document.querySelectorAll(".grid-section p");
+const imagens = document.querySelector(".imgs-social")
 
 console.log(links.length);
 console.log(paragrafos.length);
 
 paragrafos[0].classList.add("ativo");
 
+
 function activeLink(index) {
   paragrafos.forEach((paragrafo) => {
     paragrafo.classList.remove("ativo");
+    imagens.classList.remove("ativo")
   });
   paragrafos[index].classList.add("ativo");
+  if(paragrafos[paragrafos.length-1].classList.contains("ativo")){
+    imagens.classList.add("ativo")
+    //alert("awd")
+  }
 }
 
 links.forEach((link, index) => {
