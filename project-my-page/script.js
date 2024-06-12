68dc6556ea11a9facfcc4d35423e2606ac093bb1
HemersonGarcias <71848645+HemersonGarcias@users.noreply.github.com>
2024-06-12 18:24:05 -0300

    shadow mobile fix

--- project-my-page/script.js
+++ project-my-page/script.js
@@ -1,21 +1,40 @@
-const links = document.querySelectorAll("a");
-const paragrafos = document.querySelectorAll(".grid-section p");
 
-console.log(links.length);
-console.log(paragrafos.length);
+function activeParagrafo() {
+  const links = document.querySelectorAll("a");
+  const paragrafos = document.querySelectorAll(".grid-section p");
 
-paragrafos[0].classList.add("ativo");
+  paragrafos[0].classList.add("ativo");
 
+  function activeLink(index) {
+    paragrafos.forEach((paragrafo) => {
+      paragrafo.classList.remove("ativo");
+    });
+    paragrafos[index].classList.add("ativo");
+  }
 
-function activeLink(index) {
-  paragrafos.forEach((paragrafo) => {
-    paragrafo.classList.remove("ativo");
+  links.forEach((link, index) => {
+    link.addEventListener("click", () => {
+      activeLink(index);
+    });
   });
-  paragrafos[index].classList.add("ativo");
 }
+activeParagrafo();
 
-links.forEach((link, index) => {
-  link.addEventListener("click", () => {
-    activeLink(index);
-  });
-});
+function activeItem(){
+  const lis = document.querySelectorAll("li");
+  lis[0].classList.add("ativada");
+
+  function activeP(index){
+    lis.forEach((li)=>{
+      li.classList.remove("ativada");
+    })
+    lis[index].classList.add("ativada");
+  }
+
+  lis.forEach((li, index)=>{
+    li.addEventListener('click',()=>{
+      activeP(index);
+    })
+  })
+}
+activeItem();
